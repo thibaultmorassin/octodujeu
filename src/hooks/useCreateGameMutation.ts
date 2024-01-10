995dd24f3f777c6c9559d5fa8fb80443da983702
Thibault Morassin <thibault@kriptown.com>
2024-01-11 00:27:40 +0100

    feat: generate QR code in create game mutation

--- src/hooks/useCreateGameMutation.ts
+++ src/hooks/useCreateGameMutation.ts
@@ -1,14 +1,30 @@
 import { CreateGameArguments } from "@/components/game/create-game-dialog";
-import { DatabaseCollection, gameCollection } from "@/lib/firebase";
-import { addDoc } from "firebase/firestore";
+import { DatabaseCollection, db, gameCollection } from "@/lib/firebase";
+import { addDoc, doc, setDoc } from "firebase/firestore";
 import useSWRMutation from "swr/mutation";
+import QRCode from "qrcode";
 
 const useCreateGameMutation = (optimisticData?: any) => {
   const mutation = useSWRMutation(
     DatabaseCollection.game,
     async (_url: string, { arg }: { arg: CreateGameArguments }) => {
-      const docRef = await addDoc(gameCollection, arg);
-      return { id: docRef.id, ...arg, nameLowercase: arg.name.toLowerCase() };
+      try {
+        const docRef = await addDoc(gameCollection, arg);
+        const generatedQRCode = await QRCode.toDataURL(docRef.id);
+        await setDoc(doc(db, DatabaseCollection.game, docRef.id), {
+          ...arg,
+          qrCode: generatedQRCode,
+        });
+
+        return {
+          id: docRef.id,
+          ...arg,
+          nameLowercase: arg.name.toLowerCase(),
+          qrCode: generatedQRCode,
+        };
+      } catch (error) {
+        console.log(error);
+      }
     },
     { optimisticData },
   );
