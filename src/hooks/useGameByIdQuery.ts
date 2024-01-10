f2d69d9e25024dc88498322cd3772691846e426f
Thibault Morassin <thibault@kriptown.com>
2024-01-11 00:41:27 +0100

    fix: giving each SWR query a unique name

--- src/hooks/useGameByIdQuery.ts
+++ src/hooks/useGameByIdQuery.ts
@@ -3,27 +3,24 @@ import { doc, getDoc } from "firebase/firestore";
 import useSWR from "swr";
 
 const useGameByIdQuery = (gameId: string) => {
-  const { data, isLoading, error } = useSWR(
-    DatabaseCollection.game,
-    async () => {
-      const docRef = doc(db, DatabaseCollection.game, gameId);
-      const docSnap = await getDoc(docRef);
-      const gameData = docSnap.data();
+  const query = useSWR(`${DatabaseCollection.game}/${gameId}`, async () => {
+    const docRef = doc(db, DatabaseCollection.game, gameId);
+    const docSnap = await getDoc(docRef);
+    const gameData = docSnap.data();
 
-      if (gameData?.storage) {
-        const storage = await getDoc(gameData.storage);
-        const storageData = storage.data() as Object;
-        return {
-          ...docSnap.data(),
-          storage: { id: storage.id, ...storageData },
-        };
-      }
+    if (gameData?.storage) {
+      const storage = await getDoc(gameData.storage);
+      const storageData = storage.data() as Object;
+      return {
+        ...docSnap.data(),
+        storage: { id: storage.id, ...storageData },
+      };
+    }
 
-      return docSnap.data();
-    },
-  );
+    return docSnap.data();
+  });
 
-  return { data, isLoading, error };
+  return query;
 };
 
 export default useGameByIdQuery;
