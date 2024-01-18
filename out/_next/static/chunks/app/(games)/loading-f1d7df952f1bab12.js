d34d48498e809fac796ddedfa86c9eba0cc1475a
Thibault Morassin <thibault@kriptown.com>
2024-01-18 18:20:40 +0100

    build: app build v0.4

---
+++ out/_next/static/chunks/app/(games)/loading-f1d7df952f1bab12.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[901],{5974:function(n,e,u){Promise.resolve().then(u.bind(u,2673))}},function(n){n.O(0,[358,310,61,672,216,953,961,673,971,938,744],function(){return n(n.s=5974)}),_N_E=n.O()}]);
